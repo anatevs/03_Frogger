77c85a23f8ab6084d7339c781145f47183a8000e
anatevs <anatevs@gmail.com>
2025-08-25 15:10:29 +0300

    import and adjust a turtle model

---
+++ Assets/GameCore/Enviroment/MovingItems/TurtleView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class TurtleView : MonoBehaviour
+    {
+
+        private float _colliderWaterShift = -0.06f;
+
+        private float[] _waterPosY = {-0.07f, -0.14f };
+
+
+    }
+}
