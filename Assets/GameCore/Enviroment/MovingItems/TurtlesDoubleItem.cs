1cfecabead459c1a3789ebb48b68aec7190c010b
anatevs <anatevs@gmail.com>
2025-08-28 17:27:31 +0300

    addition to prev

---
+++ Assets/GameCore/Enviroment/MovingItems/TurtlesDoubleItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class TurtlesDoubleItem : MovingItem
+    {
+        [SerializeField]
+        private TurtleView[] _turtleViews;
+
+        [SerializeField]
+        private float _changeDuration;
+
+        [SerializeField]
+        private float _stayDuration;
+
+        [SerializeField]
+        private Transform _viewTransform;
+
+        private readonly float _lookRot = 180;
+
+        protected override void SetLookDirection()
+        {
+            var rotPoint = transform.position + Vector3.right * _boxCollider.center.x;
+
+            _viewTransform.RotateAround(rotPoint, Vector3.up, _lookRot);
+
+            _lookDirection = _moveDirectionX;
+        }
+    }
+}
