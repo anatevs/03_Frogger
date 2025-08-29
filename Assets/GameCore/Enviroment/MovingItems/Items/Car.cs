ac3432021912aa01c9dce4e6f49d38870cafd3b4
anatevs <anatevs@gmail.com>
2025-08-29 19:37:27 +0300

    forlders organization, prepare Car script

---
+++ Assets/GameCore/Enviroment/MovingItems/Items/Car.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace GameCore
+{
+    public class Car : MovingItem
+    {
+        [SerializeField]
+        private Transform _viewTransform;
+
+        private readonly float _lookRot = 180;
+
+        protected override void SetLookDirection()
+        {
+            var rotPoint = transform.position + Vector3.right * _boxCollider.center.x;
+
+            _viewTransform.RotateAround(rotPoint, Vector3.up, _lookRot);
+
+            _lookDirection = _moveDirectionX;
+        }
+    }
+}
